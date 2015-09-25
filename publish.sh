c69f8132d62a2bd26fc3cafcb973f60844dd2f8f
jakoch <jakoch@web.de>
2015-09-25 17:59:42 +0200

    install asciidoctor, added publish script for gh-pages, added basic book.asciidoc to see if rendering works

---
+++ publish.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git fetch origin
+  git checkout -b gh-pages
+  git add . *.html
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/WPN-XM/docs > /dev/null 2>&1
+  git push -f --set-upstream origin-pages gh-pages
+}
+
+setup_git
+commit_website_files
+upload_files
