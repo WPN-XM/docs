995f0463edaaf2bac3efea95487f5fb182b5a13a
jakoch <jakoch@web.de>
2016-10-01 11:46:59 +0200

    chmod +x

--- _travis/install_plantuml.sh
+++ _travis/install_plantuml.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+#
+# If you get a "Permission denied" error:
+# git update-index install_plantuml.sh --chmod=+x
+#
+
 wget --retry-connrefused --no-check-certificate http://ftp.stw-bonn.de/debian/pool/main/p/plantuml/plantuml_8039-1_all.deb
 sudo dpkg -i plantuml_8039-1_all.deb
 rm -rf plantuml_8039-1_all.deb
